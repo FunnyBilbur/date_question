c52287af328cb0843de1399f9c989c6d2b873609
FunnyBilbur <tonistark1997@gmail.com>
2023-12-19 02:52:59 +0300

    +

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
+const question1 = document.querySelector(".question1");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Yay, see you on the 18th!";
+    question.innerHTML = "See you on 24th of Dec";
+    question1.innerHTML = "Анечка🥰";
     gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
     //"https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWp1dmtvODRvcjFjNXo1ZGJ3MWdzazlwamJ3ZGN6OGV3Y2c3aGNsZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/x28cIQSn19Tbi/giphy.gif";
     //"https://tenor.com/view/peach-and-goma-gif-7882899598842933350.gif";
